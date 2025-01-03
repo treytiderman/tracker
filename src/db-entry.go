2a977ea7e5be8071cce493c10a7ff39911b68103
trey <tiderman.trey@gmail.com>
2025-01-02 19:16:00 -0600

    add Get_Log and test

--- src/db-entry.go
+++ src/db-entry.go
@@ -399,7 +399,42 @@ func Get_All_Entries(db *sql.DB) ([]Db_Entry, error) {
 	return entries, nil
 }
 
-// func Get_Log(db *sql.DB, log_id int) (Db_Log, error) {}
+func Get_Log(db *sql.DB, log_id int) (Db_Log, error) {
+	row := db.QueryRow(
+		`SELECT
+			IFNULL(log.log_id, 0) AS log_id,
+			IFNULL(log.log_value, 0) AS log_value,
+			IFNULL(field.field_id, 0) AS field_id,
+			IFNULL(field.field_type, "") AS field_type,
+			IFNULL(field.field_name, "") AS field_name,
+			IFNULL(number.decimal_places, 0) AS decimal_places,
+			IFNULL(option.option_value, 0) AS option_value,
+			IFNULL(option.option_name, "") AS option_name,
+			IFNULL((CASE WHEN field.field_type == "number" THEN
+				printf(("%." || number.decimal_places || "f"), log.log_value / power(10, number.decimal_places))
+			ELSE
+				option.option_name
+			END), "") AS present
+		FROM entry
+		LEFT JOIN log USING (entry_id)
+		LEFT JOIN field USING (field_id)
+		LEFT JOIN number USING (field_id)
+		LEFT JOIN option ON log.field_id = option.field_id AND log.log_value = option.option_value
+		WHERE log.log_id = ?
+		ORDER BY field.field_id;`,
+		log_id)
+
+	var log_scan Db_Log
+
+	err := row.Scan(
+		&log_scan.Id, &log_scan.Value, &log_scan.Field_Id, &log_scan.Field_Type, &log_scan.Field_Name,
+		&log_scan.Decimal_Places, &log_scan.Option_Value, &log_scan.Option_Name, &log_scan.Present)
+	if err != nil {
+		return log_scan, err
+	}
+
+	return log_scan, nil
+}
 
 // Update
 
--- src/db-entry_test.go
+++ src/db-entry_test.go
@@ -294,6 +294,130 @@ func Test_Get_Entries_Filter(t *testing.T) {
 	}
 }
 
+func Test_Get_All_Entries(t *testing.T) {
+	_test_Reset_Entry_Database(t)
+
+	journal_id, _ := Create_Tracker(db_test, "Journal", "Daily journal and notes")
+	Create_Entry(db_test, journal_id, "Entry 1")
+	Create_Entry(db_test, journal_id, "Why is green sometimes blue")
+	Create_Entry(db_test, journal_id, "If Franky can be a robot maybe I can too")
+	Create_Entry(db_test, journal_id, "")
+	Create_Entry(db_test, journal_id, "I got lost in a square")
+	Create_Entry(db_test, journal_id, "The circle showed me the way")
+
+	money_id, _ := Create_Tracker(db_test, "Money", "Transactions")
+	money_amount_id, _ := Add_Number_Field(db_test, money_id, "Amount", "Amount of money in dollars", 2)
+	money_card_id, _ := Add_Option_Field(db_test, money_id, "Card", "Payment Method")
+	Add_Option_to_Field(db_test, money_card_id, 1, "Discover")
+	Add_Option_to_Field(db_test, money_card_id, 2, "Visa")
+	Add_Option_to_Field(db_test, money_card_id, 3, "American Express")
+	money_entry_1, _ := Create_Entry(db_test, money_id, "9.99 dollars entered as 999")
+	Add_Log_To_Entry(db_test, money_entry_1, money_amount_id, -999)
+	Add_Log_To_Entry(db_test, money_entry_1, money_card_id, 1)
+	money_entry_2, _ := Create_Entry(db_test, money_id, "not for what you think")
+	Add_Log_To_Entry(db_test, money_entry_2, money_amount_id, -42069)
+	Add_Log_To_Entry(db_test, money_entry_2, money_card_id, 3)
+	money_entry_3, _ := Create_Entry(db_test, money_id, "big spendin")
+	Add_Log_To_Entry(db_test, money_entry_3, money_amount_id, 2000_00)
+	Add_Log_To_Entry(db_test, money_entry_3, money_card_id, 2)
+
+	// Test Start
+	entries, err := Get_All_Entries(db_test)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// s, _ := json.MarshalIndent(entries, "", "    ")
+	// fmt.Println("JSON:", string(s))
+
+	if entries[0].Id != 9 {
+		t.Errorf("got %d, expected %d", entries[0].Id, 9)
+	}
+
+	if entries[2].Notes != "9.99 dollars entered as 999" {
+		t.Errorf("got %s, expected %s", entries[2].Notes, "9.99 dollars entered as 999")
+	}
+
+	if entries[1].Logs[0].Present != "-420.69" {
+		t.Errorf("got %s, expected %s", entries[1].Logs[0].Present, "-420.69")
+	}
+
+	if entries[1].Logs[1].Present != "American Express" {
+		t.Errorf("got %s, expected %s", entries[1].Logs[1].Present, "American Express")
+	}
+
+	if entries[8].Notes != "Entry 1" {
+		t.Errorf("got %s, expected %s", entries[8].Notes, "Entry 1")
+	}
+
+	if entries[8].Tracker_Id != 1 {
+		t.Errorf("got %d, expected %d", entries[8].Tracker_Id, 1)
+	}
+
+}
+
+func Test_Get_Log(t *testing.T) {
+	_test_Reset_Entry_Database(t)
+
+	money_id, _ := Create_Tracker(db_test, "Money", "Transactions")
+	money_amount_id, _ := Add_Number_Field(db_test, money_id, "Amount", "Amount of money in dollars", 2)
+	money_card_id, _ := Add_Option_Field(db_test, money_id, "Card", "Payment Method")
+	Add_Option_to_Field(db_test, money_card_id, 1, "Discover")
+	Add_Option_to_Field(db_test, money_card_id, 2, "Visa")
+	Add_Option_to_Field(db_test, money_card_id, 3, "American Express")
+	money_entry_1, _ := Create_Entry(db_test, money_id, "9.99 dollars entered as 999")
+	log1_id, _ := Add_Log_To_Entry(db_test, money_entry_1, money_amount_id, -999)
+	log2_id, _ := Add_Log_To_Entry(db_test, money_entry_1, money_card_id, 1)
+	money_entry_2, _ := Create_Entry(db_test, money_id, "not for what you think")
+	Add_Log_To_Entry(db_test, money_entry_2, money_amount_id, -42069)
+	Add_Log_To_Entry(db_test, money_entry_2, money_card_id, 3)
+	money_entry_3, _ := Create_Entry(db_test, money_id, "big spendin")
+	Add_Log_To_Entry(db_test, money_entry_3, money_amount_id, 2000_00)
+	Add_Log_To_Entry(db_test, money_entry_3, money_card_id, 2)
+
+	// Test Start
+	log1, err := Get_Log(db_test, log1_id)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// s, _ := json.MarshalIndent(log1, "", "    ")
+	// fmt.Println("JSON:", string(s))
+
+	if log1.Id != 1 {
+		t.Errorf("got %d, expected %d", log1.Id, 1)
+	}
+
+	if log1.Value != -999 {
+		t.Errorf("got %d, expected %d", log1.Value, -999)
+	}
+
+	if log1.Present != "-9.99" {
+		t.Errorf("got %s, expected %s", log1.Present, "-9.99")
+	}
+
+	log2, err := Get_Log(db_test, log2_id)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// s2, _ := json.MarshalIndent(log2, "", "    ")
+	// fmt.Println("JSON:", string(s2))
+
+	if log2.Id != 2 {
+		t.Errorf("got %d, expected %d", log2.Id, 1)
+	}
+
+	if log2.Value != 1 {
+		t.Errorf("got %d, expected %d", log2.Value, 1)
+	}
+
+	if log2.Present != "Discover" {
+		t.Errorf("got %s, expected %s", log2.Present, "Discover")
+	}
+
+}
+
 // Update
 
 func Test_Update_Entry_Timestamp(t *testing.T) {
